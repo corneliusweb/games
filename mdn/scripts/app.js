bcdfc8fa64ec9cdadb321acba731f2a81be0ccee
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-09-26 16:19:30 +0100

    Define randomNumber generation and select html elements

---
+++ mdn/scripts/app.js
@@ -0,0 +1,8 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guessInput = document.querySelector('.guess-input');
+const guessSubmit = document.querySelector('.guess-submit');
+
+const guesses = document.querySelector('.guesses');
+const lastGuess = document.querySelector('.last-guess');
+const lowOrHigh = document.querySelector('.low-or-hi');
