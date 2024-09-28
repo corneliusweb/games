0bc0864be99d0f2f03c4d18645f9c33f9c0ba302
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-09-28 18:12:49 +0100

    Add some major features in Guess 2.0
    This commit carries the full styles of the Guess 2.0.
    In app.js, it adds a feature that dynamically changes the appearance
    of disabled elements.
    It adds a feature that pauses the Guesses Left and the maxAttempts count
    when a player enters an invalid value.

--- mdn_games/number-guess2/scripts/app.js
+++ mdn_games/number-guess2/scripts/app.js
@@ -1,64 +1,131 @@
-// define dynamic range
+// generate dynamic range
 const randomStart = Math.floor(Math.random() * 100) + 1;
 const randomEnd = Math.floor(Math.random() * 500) + 500;
 let randomNumber = Math.floor(
 	Math.random() * (randomEnd - randomStart + 1) + randomStart
 );
+// for testing
+// let randomNumber = 400;
 
 // select global html elements
 const leftGuessesEl = document.querySelector('.leftGuessesEl');
 let timer;
+let restartButton;
 
-const numberEl = document.querySelector('.numberEl');
+const inputEl = document.querySelector('.inputEl');
 const submitEl = document.querySelector('.submitEl');
 
+const gameStatesEl = document.querySelector('.gameStatesEl');
 const prvGuessEl = document.querySelector('.prvGuessEl');
 const guessOutcomeEl = document.querySelector('.guessOutcomeEl');
 const differenceEl = document.querySelector('.differenceEl');
 
-let attemptCount = 1;
+let attemptsCount = 1;
 let maxAttempt = 10;
 leftGuessesEl.textContent = maxAttempt;
 
-function checkGuess() {
-	const userGuess = Number(numberEl.value);
+document
+	.querySelector('form')
+	.addEventListener('submit', (e) => e.preventDefault());
 
-	if (attemptCount === 1) {
-		prvGuessEl.textContent = 'Previous guesses:';
-	}
+function checkGuess() {
+	const userGuess = Number(inputEl.value);
+
 	prvGuessEl.textContent += ` ${userGuess}`;
 
 	if (userGuess === randomNumber) {
+		setGameOver();
 		differenceEl.textContent = '';
-		numberEl.disabled = true;
-		submitEl.disabled = true;
 
-		if (attemptCount < 5) {
+		if (attemptsCount < 5) {
 			guessOutcomeEl.textContent = `That was a crazy move! You're a genius!!!`;
 		} else {
 			guessOutcomeEl.textContent = `That's correct! Congrats!!`;
 		}
-	} else if (attemptCount === 10) {
+	} else if (attemptsCount === 10) {
+		setGameOver();
 		differenceEl.textContent = '';
-		numberEl.disabled = true;
-		submitEl.disabled = true;
 		guessOutcomeEl.textContent = '!!!GAME OVER!!!';
 	} else {
 		const difference = Math.abs(randomNumber - userGuess);
 		if (difference < 10) {
 			differenceEl.textContent = `That was a close call! You got this champ!`;
-		} else if (difference < 50) {
-			differenceEl.textContent = `You're not far from home. Trust me, you got this!`;
-		} else if (difference >= 50) {
+		} else if (difference <= 50) {
+			differenceEl.textContent = `You're not far from home. You got this!`;
+		} else if (difference > 50) {
 			differenceEl.textContent = `You're far away! Go again.`;
+		}
+		guessOutcomeEl.textContent = 'Wrong!';
+
+		if (inputEl.value > 1000 || inputEl.value < 1) {
+			prvGuessEl.textContent = '';
+         guessOutcomeEl.textContent = 'Invalid! Pick numbers between 1 - 1000';
+         
+			if (maxAttempt !== 10) {
+				++maxAttempt;
+			} else {
+            maxAttempt += 1;
+			}
+         --attemptsCount;
 		}
 	}
 
-	attemptCount++;
+	inputEl.value = '';
+	attemptsCount++;
 	const leftGuesses = --maxAttempt;
 	leftGuessesEl.textContent = leftGuesses;
 }
 
 submitEl.addEventListener('click', checkGuess);
 
+function setGameOver() {
+	restartButton = document.createElement('button');
+	restartButton.textContent = 'Restart Game';
+	gameStatesEl.appendChild(restartButton);
+	restartButton.setAttribute('class', 'restartButtonEl');
 
+	setDisabledEl();
+
+	restartButton.addEventListener('click', restartGame);
+}
+
+function restartGame() {
+	const paras = document.querySelectorAll('.gameStatesEl p');
+	for (const para of paras) {
+		para.textContent = '';
+	}
+	prvGuessEl.textContent = '';
+
+	attemptsCount = 1;
+	maxAttempt = 10;
+	inputEl.value = '';
+	inputEl.focus();
+	leftGuessesEl.textContent = 10;
+
+	gameStatesEl.removeChild(restartButton);
+
+	randomNumber =
+		Math.floor(Math.random() * (randomEnd - randomStart + 1)) + randomStart;
+
+	unsetDisabledEl();
+}
+
+function setDisabledEl() {
+	inputEl.disabled = true;
+	inputEl.style.cursor = 'not-allowed';
+	inputEl.style.opacity = '0.6';
+
+	submitEl.disabled = true;
+	submitEl.style.cursor = 'not-allowed';
+	submitEl.style.opacity = '0.6';
+}
+
+function unsetDisabledEl() {
+	inputEl.disabled = false;
+	inputEl.style.cursor = 'text';
+	inputEl.style.opacity = '1';
+
+	submitEl.disabled = false;
+	submitEl.style.cursor = 'pointer';
+	submitEl.style.opacity = '1';
+}
