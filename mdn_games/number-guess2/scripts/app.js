ed96d3065d6b7423e7beed64e64b5897e31b4f5c
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-09-30 13:47:10 +0100

    Add modal functons, action button functions and adjust css animation

--- mdn_games/number-guess2/scripts/app.js
+++ mdn_games/number-guess2/scripts/app.js
@@ -1,20 +1,20 @@
 // generate dynamic range
-const randomStart = Math.floor(Math.random() * 100) + 1;
-const randomEnd = Math.floor(Math.random() * 500) + 500;
-let randomNumber = Math.floor(
-	Math.random() * (randomEnd - randomStart + 1) + randomStart
-);
+const lowestRandom = Math.floor(Math.random() * 100) + 1;
+const mediumRandom = Math.floor(Math.random() * 250) + 250;
+const highestRandom = Math.floor(Math.random() * 500) + 500;
+
+let randomNumber = lowestRandom;
 // for testing
 // let randomNumber = 400;
 
 // select global html elements
-const leftGuessesEl = document.querySelector('.leftGuessesEl');
 let timer;
 let restartButton;
 
 const inputEl = document.querySelector('.inputEl');
 const submitEl = document.querySelector('.submitEl');
 
+const leftGuessesEl = document.querySelector('.leftGuessesEl');
 const gameOverBtnEl = document.querySelector('.gameOverBtnEl');
 const prvGuessEl = document.querySelector('.prvGuessEl');
 const gameStatesEl = document.querySelector('.gameStatesEl');
@@ -30,10 +30,10 @@ document
 	.addEventListener('submit', (e) => e.preventDefault());
 
 function checkGuess() {
-   const userGuess = Number(inputEl.value);
-   
-   prvGuessEl.textContent += ` ${userGuess}`;
-   guessOutcomeEl.style.display = 'block'
+	const userGuess = Number(inputEl.value);
+
+	prvGuessEl.textContent += ` ${userGuess}`;
+	guessOutcomeEl.style.display = 'block';
 
 	if (userGuess === randomNumber) {
 		setGameOver();
@@ -82,6 +82,78 @@ function checkGuess() {
 
 submitEl.addEventListener('click', checkGuess);
 
+const gameOverModal = document.querySelector('.gameOverEl');
+const gameLevelModal = document.querySelector('.gameLevelsEl');
+const changeLevelBtn = document.querySelectorAll('.changeLevelEl');
+
+function showLevel() {
+	for (const button of changeLevelBtn) {
+		button.addEventListener('click', () => {
+			gameLevelModal.style.display = 'flex';
+			gameOverModal.style.display = 'none';
+
+			const paras = document.querySelectorAll('.gameStatesEl p');
+			for (const para of paras) {
+				para.textContent = '';
+			}
+			prvGuessEl.textContent = '';
+
+			attemptsCount = 1;
+			maxAttempt = 10;
+			inputEl.value = '';
+			guessOutcomeEl.style.display = 'none';
+			inputEl.focus();
+			leftGuessesEl.textContent = 10;
+			setDisabledEl();
+		});
+	}
+}
+showLevel();
+
+// set game levels
+function setEasy() {
+	randomNumber = lowestRandom;
+}
+document.querySelector('.levels .easy').addEventListener('click', () => {
+	setEasy();
+	gameLevelModal.style.display = 'none';
+});
+
+function setBalanced() {
+	randomNumber =
+		Math.floor(Math.random() * (mediumRandom - lowestRandom + 1)) +
+		lowestRandom;
+}
+document.querySelector('.levels .balanced').addEventListener('click', () => {
+	setBalanced();
+	gameLevelModal.style.display = 'none';
+});
+
+function setMindReader() {
+	randomNumber =
+		Math.floor(Math.random() * (highestRandom - lowestRandom + 1)) +
+		lowestRandom;
+}
+document.querySelector('.levels .mind-reader').addEventListener('click', () => {
+	setMindReader();
+	gameLevelModal.style.display = 'none';
+});
+
+const helpBtn = document.querySelector('.helpBtnEl');
+const helpModal = document.querySelector('.helpEl');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	helpModal.style.display = 'flex';
+	setDisabledEl();
+});
+
+document.querySelector('.helpEl .close').addEventListener('click', () => {
+	helpModal.style.display = 'none';
+});
+helpBtn.addEventListener('click', () => {
+	helpModal.style.display = 'flex';
+});
+
 function setGameOver() {
 	restartButton = document.createElement('button');
 	restartButton.textContent = 'Restart Game';
@@ -89,6 +161,9 @@ function setGameOver() {
 	restartButton.textContent = 'Restart Game';
 	gameOverBtnEl.appendChild(restartButton);
 
+	gameOverModal.style.display = 'flex';
+
+	enabledStartBtn();
 	setDisabledEl();
 
 	restartButton.addEventListener('click', restartGame);
@@ -104,25 +179,25 @@ function restartGame() {
 	attemptsCount = 1;
 	maxAttempt = 10;
 	inputEl.value = '';
+	guessOutcomeEl.style.display = 'none';
 	inputEl.focus();
 	leftGuessesEl.textContent = 10;
 
 	gameOverBtnEl.removeChild(restartButton);
+	gameOverModal.style.display = 'none';
 
-	randomNumber =
-		Math.floor(Math.random() * (randomEnd - randomStart + 1)) + randomStart;
-
+	showLevel();
 	unsetDisabledEl();
 }
 
 function setDisabledEl() {
 	inputEl.disabled = true;
 	inputEl.style.cursor = 'not-allowed';
-	inputEl.style.opacity = '0.6';
+	inputEl.style.opacity = '0.4';
 
 	submitEl.disabled = true;
 	submitEl.style.cursor = 'not-allowed';
-	submitEl.style.opacity = '0.6';
+	submitEl.style.opacity = '0.4';
 }
 
 function unsetDisabledEl() {
@@ -133,4 +208,26 @@ function unsetDisabledEl() {
 	submitEl.disabled = false;
 	submitEl.style.cursor = 'pointer';
 	submitEl.style.opacity = '1';
+}
+
+const startBtn = document.querySelector('.startGameEl');
+
+function startGame() {
+	unsetDisabledEl();
+	disableStartBtn();
+}
+startBtn.addEventListener('click', () => {
+	startGame();
+});
+
+function disableStartBtn() {
+	startBtn.disabled = true;
+	startBtn.style.cursor = 'not-allowed';
+	startBtn.style.opacity = '0.3';
+}
+
+function enabledStartBtn() {
+	startBtn.disabled = false;
+	startBtn.style.cursor = 'pointer';
+	startBtn.style.opacity = '1';
 }
